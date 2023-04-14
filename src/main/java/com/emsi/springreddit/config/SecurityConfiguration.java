852a27e71124117f8751de7c07ced4b9c2aa5de9
DaddaAdam <daddaadam@gmail.com>
2023-04-14 10:52:38 +0000

    Set up Spring Security 6 configuration

---
+++ src/main/java/com/emsi/springreddit/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.emsi.springreddit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .dispatcherTypeMatchers(HttpMethod.valueOf("/api/auth/**"))
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        return httpSecurity.build();
+    }
+}
